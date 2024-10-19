93c0ad10e3796796a4842ec2dc64447792e88895
FurtulaIgor <58146193+FurtulaIgor@users.noreply.github.com>
2024-10-19 21:36:34 +0200

    Strelica UP

--- js/scripts.js
+++ js/scripts.js
@@ -16,18 +16,37 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 // Prvo pronađemo elemente
-const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('#nav-menu');
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
 
 // Otvaranje i zatvaranje menija na klik dugmeta
-hamburger.addEventListener('click', (event) => {
+hamburger.addEventListener("click", (event) => {
   event.stopPropagation(); // Sprečava zatvaranje odmah pri kliku na dugme
-  navMenu.classList.toggle('active');
+  navMenu.classList.toggle("active");
 });
 
 // Zatvaranje menija ako korisnik klikne van njega
-document.addEventListener('click', (event) => {
+document.addEventListener("click", (event) => {
   if (!navMenu.contains(event.target)) {
-    navMenu.classList.remove('active');
+    navMenu.classList.remove("active");
   }
+});
+
+const scrollToTopButton = document.getElementById("scrollToTop");
+
+// Prikaz dugmeta kada korisnik skroluje dole
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollToTopButton.classList.add("show");
+  } else {
+    scrollToTopButton.classList.remove("show");
+  }
+});
+
+// Funkcionalnost za vraćanje na vrh kada se klikne na dugme
+scrollToTopButton.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 });
